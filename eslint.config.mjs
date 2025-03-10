6431666f70711bce453fe60de8065b6435db6b2d
suman sharma <mansu7802@gmail.com>
2025-03-10 18:28:10 +0545

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,35 +5,25 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
     {
-        ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'],
         files: ['**/*.{js,mjs,cjs,ts}'],
-        languageOptions: {
-            globals: globals.browser,
-        },
-        plugins: {
-            security: eslintPluginSecurity,
-            'simple-import-sort': simpleImportSort,
-        },
+        languageOptions: { globals: globals.browser },
+        ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'],
+        plugins: { security: eslintPluginSecurity, 'simple-import-sort': simpleImportSort },
         rules: {
             'no-console': 'error',
             'no-await-in-loop': 'error',
             'no-duplicate-imports': 'error',
             'require-atomic-updates': 'error',
+            'simple-import-sort/exports': 'error',
             '@typescript-eslint/no-unused-vars': 'off',
             '@typescript-eslint/no-empty-object-type': 'off',
-            'simple-import-sort/exports': 'error',
-            'simple-import-sort/imports': [
-                'error',
-                {
-                    groups: [['^node:'], ['^express$', '^@?\\w'], ['^@'], ['^\\.']],
-                },
-            ],
+            'simple-import-sort/imports': ['error', { groups: [['^node:'], ['^express$', '^@?\\w'], ['^@'], ['^\\.']] }],
         },
-        ...pluginJs.configs.recommended,
-        ...tseslint.configs.recommended,
-        ...eslintConfigPrettier,
     },
 ];
