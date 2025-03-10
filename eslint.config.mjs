3785cc30131d264475c75f46ddb2c4267c75e2f9
suman sharma <mansu7802@gmail.com>
2025-03-10 17:30:37 +0545

    fix: ignore build dir by esling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,28 +5,25 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'], ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
     {
+        ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**'],
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        languageOptions: {
+            globals: globals.browser,
+        },
         plugins: {
             security: eslintPluginSecurity,
             'simple-import-sort': simpleImportSort,
         },
-
         rules: {
             'no-console': 'error',
             'no-await-in-loop': 'error',
             'no-duplicate-imports': 'error',
             'require-atomic-updates': 'error',
-
             '@typescript-eslint/no-unused-vars': 'off',
             '@typescript-eslint/no-empty-object-type': 'off',
-
             'simple-import-sort/exports': 'error',
             'simple-import-sort/imports': [
                 'error',
@@ -35,5 +32,8 @@ export default [
                 },
             ],
         },
+        ...pluginJs.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...eslintConfigPrettier,
     },
 ];
